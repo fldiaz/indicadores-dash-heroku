002a1fb0b4518c354f2798fe8b170f88b95c851f
fldiaz <fldiaz@itba.edu.ar>
2019-05-24 18:47:57 -0300

    'final2'

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+
 from app import app
 #from app import server
 from layouts import inflacion, Dinero, Empleo, Actividad, SectorPúblico
