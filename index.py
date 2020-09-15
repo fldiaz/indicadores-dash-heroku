ed8d8227c1d913707008476758843cd1e036cabb
Florencia Diaz <fldiaz@itba.edu.ar>
2020-09-15 18:12:21 -0300

    act

--- index.py
+++ index.py
@@ -9,29 +9,24 @@ from layouts import inflacion, Dinero, Empleo, Actividad, SectorPúblico
 import pandas as pd
 
 app.layout = html.Div([
-    dcc.Location(id='url', refresh=False,
-    children=[
-        html.Div(id='page-content',
-        children=[
-            html.Img(id="logo", src="Logo.Cross.Validated.png"),
-            html.H4(children="Rate of US Poison-Induced Deaths"),
-            html.P(
-                id="description",
-                children="† Deaths are classified using the International Classification of Diseases, \
-                Tenth Revision (ICD–10). Drug-poisoning deaths are defined as having ICD–10 underlying \
-                cause-of-death codes X40–X44 (unintentional), X60–X64 (suicide), X85 (homicide), or Y10–Y14 \
-                (undetermined intent).",
-            ),
-        ],
-        ),
-        ],
-        ),
-        ])
+    dcc.Location(id='url', refresh=False),
+        html.Div(id='page-content')])
+        #children=[
+            #html.Img(id="logo", src="Logo.Cross.Validated.png"),
+            #html.H4(children="Rate of US Poison-Induced Deaths"),
+            #html.P(
+                #id="description",
+                #children="† Deaths are classified using the International Classification of Diseases, \
+                #Tenth Revision (ICD–10). Drug-poisoning deaths are defined as having ICD–10 underlying \
+                #cause-of-death codes X40–X44 (unintentional), X60–X64 (suicide), X85 (homicide), or Y10–Y14 \
+                #(undetermined intent).",
+
 
 # App layout
 
 
 index_page = html.Div([
+    html.Img(id="logo", src="Logo.Cross.Validated.png"),
     html.H1('Indicadores Económicos de Argentina', style={'color': '#0099e5'}),
     html.Div([
         html.P(
